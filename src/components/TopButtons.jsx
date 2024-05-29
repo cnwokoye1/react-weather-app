366e16b8250ef2b4dd8359998c7986152feeb735
cnwokoye1 <155400700+cnwokoye1@users.noreply.github.com>
2024-05-29 15:59:25 -0400

    update

---
+++ src/components/TopButtons.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+
+const TopButtons = () => {
+
+    // array of objects
+    const cities = [
+        {
+            id: 1,
+            name: 'Abuja'
+        },
+        {
+            id: 2,
+            name: 'Sydney'
+        },
+        {
+            id: 3,
+            name: 'Tokyo'
+        },
+        {
+            id: 4,
+            name: 'Paris'
+        },
+        {
+            id: 5,
+            name: 'Cape Town'
+        }
+    ]
+
+    
+
+  return (
+    <div className='flex items-center justify-around my-6'>
+        {
+            cities.map((city) => (
+                <button key={city.id} 
+                className='text-lg font-medium hover:bg-gray-700/20 px-3 py-2
+        rounded-md transition ease-in'>
+                    {city.name}
+                </button>
+            ))
+        }
+    </div>
+  )
+}
+
+export default TopButtons
