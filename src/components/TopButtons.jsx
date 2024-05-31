8e7000d6876fbdb3b5af6ed5b88070354d44240f
cnwokoye1 <155400700+cnwokoye1@users.noreply.github.com>
2024-05-30 22:46:47 -0400

    update

--- src/components/TopButtons.jsx
+++ src/components/TopButtons.jsx
@@ -1,46 +1,43 @@
-import React from 'react'
+import React from "react";
 
 const TopButtons = () => {
-
-    // array of objects
-    const cities = [
-        {
-            id: 1,
-            name: 'Orlando'
-        },
-        {
-            id: 2,
-            name: 'Sydney'
-        },
-        {
-            id: 3,
-            name: 'Abuja'
-        },
-        {
-            id: 4,
-            name: 'Paris'
-        },
-        {
-            id: 5,
-            name: 'New York'
-        }
-    ]
-
-    
+  // array of objects
+  const cities = [
+    {
+      id: 1,
+      name: "Orlando",
+    },
+    {
+      id: 2,
+      name: "New York",
+    },
+    {
+      id: 3,
+      name: "Abuja",
+    },
+    {
+      id: 4,
+      name: "Dubai",
+    },
+    {
+      id: 5,
+      name: "Venice",
+    },
+  ];
 
   return (
-    <div className='flex items-center justify-around my-6'>
-        {
-            cities.map((city) => (
-                <button key={city.id} 
-                className='text-lg font-medium hover:bg-gray-700/20 px-3 py-2
-        rounded-md transition ease-in'>
-                    {city.name}
-                </button>
-            ))
-        }
+    <div className="flex items-center justify-around my-6">
+      {cities.map((city) => (
+        <button
+          key={city.id}
+          className="text-lg font-medium hover:bg-gray-700/20 px-3 py-2
+        rounded-md transition ease-in"
+        >
+          {city.name}
+        </button>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default TopButtons
+export default TopButtons;
