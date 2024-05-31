890adb49ff1077fea38cec6f950d23dc7cc9aca8
cnwokoye1 <155400700+cnwokoye1@users.noreply.github.com>
2024-05-30 22:40:16 -0400

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,24 @@
 // import component files
-import TopButtons from "./components/TopButtons"
-import Inputs from "./components/Inputs"
-import TimeAndLocation from "./components/TimeAndLocation"
-import TempAndDetails from "./components/TempAndDetails"
-import Forecast from "./components/Forecast"
-import getWeatherData from "./services/weatherService"
+import TopButtons from "./components/TopButtons";
+import Inputs from "./components/Inputs";
+import TimeAndLocation from "./components/TimeAndLocation";
+import TempAndDetails from "./components/TempAndDetails";
+import Forecast from "./components/Forecast";
+import getFormattedWeatherData from "./services/weatherService";
 
 const App = () => {
-
   const getWeather = async () => {
-    const data = await getWeatherData('weather', {q: 'berlin'})
+    const data = await getFormattedWeatherData({ q: "berlin" });
     console.log(data);
   };
-  
+
   getWeather();
 
   return (
-    <div className="mx-auto max-w-screen-lg mt-4 py-5 px-32 
-    bg-gradient-to-br shadow-xl shadow-gray-400 from-cyan-600 to-blue-700">
+    <div
+      className="mx-auto max-w-screen-lg mt-4 py-5 px-32 
+    bg-gradient-to-br shadow-xl shadow-gray-400 from-cyan-600 to-blue-700"
+    >
       <TopButtons />
       <Inputs />
 
@@ -26,7 +27,7 @@ const App = () => {
       <Forecast />
       <Forecast />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
